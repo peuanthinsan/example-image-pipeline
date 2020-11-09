b3429f3afbc621a69385ebb388e5ff8bffcdbe1c
peuanthinsan <peuanthinsan@berkeley.edu>
2020-11-09 11:42:00 -0800

    exampe holy cross image graph

---
+++ openfido.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# errexit: abort script at first error
+set -e
+
+echo "OPENFIDO_INPUT = $OPENFIDO_INPUT"
+echo "OPENFIDO_OUTPUT = $OPENFIDO_OUTPUT"
+
+path_to_tmp_dir=tmp
+
+echo "Creating tmp directory"
+mkdir -p $path_to_tmp_dir
+
+echo "Copying input files to tmp directory"
+cp -r $OPENFIDO_INPUT/* ./tmp
+
+mv $path_to_tmp_dir/* $OPENFIDO_OUTPUT  # or some but not all
