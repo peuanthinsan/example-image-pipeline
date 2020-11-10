0441cabeb68fc175a34c8a7a36ce144609f3dc0e
peuanthinsan <peuanthinsan@berkeley.edu>
2020-11-10 10:58:21 -0800

    try copying all in working directory to output

--- openfido.sh
+++ openfido.sh
@@ -7,10 +7,4 @@ set -e
 # print command to stdout before executing it:
 set -x
 
-ls $OPENFIDO_INPUT > $OPENFIDO_OUTPUT/inputfiles.txt
-
-echo "Copying input files directory"
-ls .
-mv holy-cross-15-min.png $OPENFIDO_OUTPUT
-mv holy-cross-60-min.png $OPENFIDO_OUTPUT
-
+cp -r . $OPENFIDO_OUTPUT
